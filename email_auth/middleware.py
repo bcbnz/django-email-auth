97da683197d782ec47d82e5dc957ae133ab8d0d9
Blair Bonnett <blair.bonnett@gmail.com>
2010-09-23 00:47:50 +1200

    Overload the admin login view to integrate it with backend.

--- email_auth/middleware.py
+++ email_auth/middleware.py
@@ -18,11 +18,14 @@
 from functools import wraps
 
 import django.contrib.admin.views.decorators
+import django.contrib.admin.sites
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.utils.functional import update_wrapper
 from django.utils.http import urlquote
 from django.utils.translation import ugettext_lazy as _
+from django.views.decorators.cache import never_cache
 
 
 def staff_member_required(view_func):
@@ -79,6 +82,43 @@ def staff_member_required(view_func):
 
     return wraps(view_func)(_checklogin)
 
+def admin_view(self, view, cacheable=False):
+    def inner(request, *args, **kwargs):
+        # Not logged in
+        if not request.user.is_authenticated():
+            from django.conf import settings
+            from django.contrib.auth import REDIRECT_FIELD_NAME
+            login_url = settings.LOGIN_URL
+            path = urlquote(request.get_full_path())
+            redirect_path = '%s?%s=%s' % (login_url, REDIRECT_FIELD_NAME, path)
+            return HttpResponseRedirect(redirect_path)
+
+        # Inactive user. This *should* be handled by the login form, and
+        # inactive users *shouldn't* have staff permission, but lets make
+        # absolutely sure.
+        if not request.user.is_active:
+            context = {
+                'message': _('Your user account is not active.'),
+                'title': _('Inactive account'),
+            }
+            return render_to_response('admin/inactive.html', context,
+                                      context_instance=RequestContext(request))
+
+        # Don't have permission to do this
+        if not self.has_permission(request):
+            context = {
+                'message': _('You do not have permission to view this page.'),
+                'title': _('Permission error'),
+            }
+            return render_to_response('admin/permission_error.html', context,
+                                      context_instance=RequestContext(request))
+
+        # We're good to go
+        return view(request, *args, **kwargs)
+    if not cacheable:
+        inner = never_cache(inner)
+    return update_wrapper(inner, view)
+
 class EmailAuthMiddleware:
     """Middleware to better integrate the email authentication backend into
     the admin site. The default staff_member_required decorator used to protect
@@ -99,4 +139,6 @@ class EmailAuthMiddleware:
     """
     def process_request(self, request):
         django.contrib.admin.views.decorators.staff_member_required = staff_member_required
+        django.contrib.admin.sites.AdminSite.admin_view = admin_view
+        django.contrib.admin.sites.site = django.contrib.admin.sites.AdminSite()
         return None
